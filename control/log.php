54fa530e8369646eceb3fa62837166cbd2eb0ff6
lytning <lytning@bitnp.net>
2018-04-22 18:09:03 +0800

    clean log files in repo

--- control/log.php
+++ control/log.php
@@ -1,7 +1,7 @@
 <?php
 class Log{
     public static function write($info){
-        $log = fopen("log.txt", "a");
+        $log = fopen("main.log", "a");
         fwrite($log, $info);
         fwrite($log, "\r\n" . date("Y-m-d H:i:s", time()) . "\r\n\r\n");
         fclose($log);
